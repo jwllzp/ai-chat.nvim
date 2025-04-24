620adecd95eb9b1e4652c850009c0572368a0c1d
james walls <35391305+jameswalls@users.noreply.github.com>
2025-04-24 00:21:45 -0600

    - non editable split - hide show commands

--- lua/ai_chat.lua
+++ lua/ai_chat.lua
@@ -10,7 +10,8 @@ M.state = {
 	split = {
 		buf = -1,
 		win = -1,
-	}
+	},
+	current_line = 0
 }
 
 --- create a floating window
@@ -38,10 +39,26 @@ M.open_floating_win = function(opts)
 		-- border = "rounded",
 	}
 
+	-- settings
 	local win = vim.api.nvim_open_win(buf, true, win_opts)
 	vim.api.nvim_set_option_value("wrap", true, { win = win })
+	vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
 
-	return { buf=buf, win=win }
+	return { buf=buf, win=win, }
+end
+
+--- returns each line in the response as an element in a table
+---@param prompt string
+---@param output_text string: raw string from response
+---@param split_cols integer: number of columns in split
+M.format_output = function(prompt, output_text, split_cols)
+	local sep1 = string.rep("=", split_cols)
+	local sep2 = string.rep("-", split_cols)
+	local lines = vim.split(
+		sep1 .. "\n".. prompt .. "\n".. sep2 .."\n" .. output_text, "\n",
+		{plain=true}
+	)
+	return lines
 end
 
 --- makes request to api and prints response
@@ -61,23 +78,42 @@ M.chat = function()
 	})
 	local data = vim.json.decode(res.body)
 	local output_text = data["output"][1]["content"][1]["text"]
-	local lines = vim.split(output_text, "\n", {plain=true})
 
 	-- create window if not exists
 	if not vim.api.nvim_win_is_valid(M.state.split.win) then
 		M.state.split = M.open_floating_win({ buf = M.state.split.buf })
 	end
 
-	-- clear buffer contents
-	vim.api.nvim_buf_set_lines(M.state.split.buf, 0, -1, false, {})
-
 	-- insert output
-	vim.api.nvim_buf_set_lines(M.state.split.buf, 0, #lines, false, lines)
+	local lines = M.format_output(
+		prompt,
+		output_text,
+		vim.api.nvim_win_get_width(M.state.split.win)
+	)
+	local start = M.state.current_line
+	local end_ = M.state.current_line + #lines
+	vim.api.nvim_set_option_value("modifiable", true, { buf = M.state.split.buf })
+	vim.api.nvim_buf_set_lines(M.state.split.buf, start, end_, false, lines)
+	vim.api.nvim_set_option_value("modifiable", false, { buf = M.state.split.buf })
+
+	-- focus on newest prompt
+	vim.api.nvim_win_set_cursor(M.state.split.win, {start+2, 0})
+	vim.api.nvim_command("normal! zt")
+
+	M.state.current_line = end_
 end
 
 
 vim.api.nvim_create_user_command("Aichat", function()
 	M.chat()
 end, {})
+
+vim.keymap.set("n", "<leader><leader>", function()
+	if not vim.api.nvim_win_is_valid(M.state.split.win) then
+		M.state.split = M.open_floating_win({ buf = M.state.split.buf })
+	else
+		vim.api.nvim_win_hide(M.state.split.win)
+	end
+end, { desc = "Toggle floating split" })
 
 return M
