0e6c289e1ef6ae41a677adf19fbd0005e6d97b8a
james walls <35391305+jameswalls@users.noreply.github.com>
2025-04-23 22:53:57 -0600

    add text to the right of the terminal

--- lua/ai_chat.lua
+++ lua/ai_chat.lua
@@ -6,6 +6,44 @@ M.setup = function()
 	-- nothing
 end
 
+M.state = {
+	floating = {
+		buf = -1,
+		win = -1,
+	}
+}
+
+--- create a floating window
+M.open_floating_win = function(opts)
+	opts = opts or {}
+	local win_width = math.floor((opts.width or 0.5) * vim.o.columns)
+	local win_height = math.floor((opts.height or 0.5) * vim.o.lines)
+	local row = math.floor((vim.o.lines - win_height) / 2)
+	local col = math.floor((vim.o.columns - win_width) / 2)
+
+	local buf = nil
+	if vim.api.nvim_buf_is_valid(opts.buf) then
+		buf = opts.buf
+	else
+		buf = vim.api.nvim_create_buf(false, true)
+	end
+
+	local win_opts = {
+		split = "right",
+		width = win_width,
+		height = win_height,
+		-- row = row,
+		-- col = col,
+		style = "minimal",
+		-- border = "rounded",
+	}
+
+	local win = vim.api.nvim_open_win(buf, true, win_opts)
+	vim.api.nvim_set_option_value("wrap", true, { win = win })
+
+	return { buf=buf, win=win }
+end
+
 --- makes request to api and prints response
 ---@return nil: api response
 M.chat = function()
@@ -22,15 +60,24 @@ M.chat = function()
 		}),
 	})
 	local data = vim.json.decode(res.body)
-	print(vim.inspect(data))
+	local output_text = data["output"][1]["content"][1]["text"]
+	local lines = vim.split(output_text, "\n", {plain=true})
+
+	-- create window if not exists
+	if not vim.api.nvim_win_is_valid(M.state.floating.win) then
+		M.state.floating = M.open_floating_win({ buf = M.state.floating.buf })
+	end
+
+	-- clear buffer contents
+	vim.api.nvim_buf_set_lines(M.state.floating.buf, 0, -1, false, {})
+
+	-- insert output
+	vim.api.nvim_buf_set_lines(M.state.floating.buf, 0, #lines, false, lines)
 end
 
-vim.api.nvim_create_user_command("AiChat", function(opts)
-  if opts.args == "chat" then
-    M.chat()
-  else
-    print("Unknown subcommand: " .. opts.args)
-  end
-end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("Aichat", function()
+	M.chat()
+end, {})
 
 return M
